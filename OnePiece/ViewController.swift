49a3a2235f66d0d21db68c0946e182ed2222e2f8
黃孝先 <seven579@hotmail.com>
2018-07-12 00:43:04 +0800

    Initial Commit

---
+++ OnePiece/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OnePiece
+//
+//  Created by HsiaoHsien Huang on 2018/7/12.
+//  Copyright © 2018年 HsiaoHsien Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
